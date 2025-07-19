c2f8d11136731aea4f245fdb917126c744dc6e19
unknown <talukdarudit69@gmail.com>
2025-07-19 15:28:27 +0530

    CORS Update

--- backend/src/index.js
+++ backend/src/index.js
@@ -11,7 +11,25 @@ const PORT = process.env.PORT;
 const __dirname = path.resolve();
 
 const app = express();
-app.use(cors({ origin: "https://leaderboard-app-frontend-mb1l.onrender.com", credentials: true }));
+
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://leaderboard-app-frontend-mb1l.onrender.com"
+];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    // allow requests with no origin (like mobile apps, curl, etc.)
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.includes(origin)) {
+      return callback(null, true);
+    } else {
+      return callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true
+}));
+
 app.use(express.json());
 
 app.use("/api", userRoutes);
